d0ecb755d2b04b9c2542f2da6332b25095f37551
phucbui <laptaolap@gmail.com>
2025-04-20 21:14:21 +0700

    fix code

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-
+const link_cn_mgbd = process.env.LINK_CN_MGDB;
 async function connectDB() {
     try {
-        await mongoose.connect('mongodb+srv://phucbui:0TLY2m8xHlQpUbw1@cluster0.pr6pa3d.mongodb.net/webbio');
+        await mongoose.connect(link_cn_mgbd);
         console.log('✅ Kết nối MongoDB thành công');
     } catch (error) {
         console.log('❌ Lỗi kết nối MongoDB:', error.message);
