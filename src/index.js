384b7367f4104649482df5bb1ba63360a53f6fb0
phucbui <laptaolap@gmail.com>
2025-04-21 14:21:09 +0700

    used cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
@@ -8,7 +9,12 @@ const route = require('./routes');
 
 const port = process.env.PORT || 3000;
 
+const corsOptions = {
+  origin: ['http://localhost:5173', 'https://01-react-taupe.vercel.app/'],
+  methods: ['GET', 'POST'],
+};
 
+app.use(cors(corsOptions));
 app.use(morgan('combined'));
 const db = require('./config/db/index')
 db.connectDB()
